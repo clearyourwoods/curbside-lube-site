a7f5ed9e171528f2caf53fde9c70c6e6168d038e
clearyourwoods <tylercoleman15@hotmail.com>
2025-09-05 14:25:34 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/curbside-lube-site/', // Adjust if using GitHub Project Pages
-})
